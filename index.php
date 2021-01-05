d8ee24a45da3f07c7f61749443964c9437f2446c
yangzhaohang <yangzhaohang@comsenz-service.com>
2021-01-05 14:14:22 +0800

    去除敏感信息

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 use Huoban\Models\HuobanItem;
-use Huoban\Helpers\Tools;
 use Huoban\Models\HuobanApplication;
 
 defined('IS_TEST') or define('IS_TEST', false);
 
-$ticket = HuobanApplication::getEnterpriseTicket('1000307','GkCtOwFXsr1Sqsne6TNi0gMmwHZxKqTn9AzLyuEw');
+$application_id = '';
+$application_secret = '';
+$ticket = HuobanApplication::getEnterpriseTicket($application_id, $application_secret);
 HuobanApplication::setTicket($ticket);
 
 $res = HuobanItem::get('2300001282173559');
